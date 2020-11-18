22a1f5a853580bb7d7b6d9c7b83e8a341247a939
StanislasE <stanislas.etienne@gmail.com>
2020-11-18 15:08:49 +0100

    fait

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+    $object = 'Yo tout le monde' ;  // Déclaration des variables
+    $message = 'Comment allez-vous ?' ;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1</title>
+</head>
+<body>
+
+    <p> 
+        <?php 
+            echo $object ;
+            echo $message ;
+        ?> 
+    </p>
+    
+</body>
+</html>
